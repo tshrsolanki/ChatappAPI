8724a1982afe63500c7e9b53f9a9b85c4f53cb93
tshrsolanki <81178283+tshrsolanki@users.noreply.github.com>
2023-01-22 22:34:48 +0530

    Update connectDB.js

--- connectDB.js
+++ connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(
-      "mongodb+srv://tushar:chatapp769843@chatapp.wlr6kkc.mongodb.net/chatapp?retryWrites=true&w=majority"
+      ""
     );
     console.log(`mongo connected`);
   } catch (error) {
